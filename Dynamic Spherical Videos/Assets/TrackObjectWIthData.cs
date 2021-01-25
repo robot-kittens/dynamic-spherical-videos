98a49d3a0e55a560ac40b1de52431ed0779d4e53
Patrick van der Werf <patrick@robotkittens.nl>
2021-01-25 23:19:13 +0100

    Implemented trackingdata for c# -- Converts trackingdata from JSON to C#

---
+++ Dynamic Spherical Videos/Assets/TrackObjectWIthData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TrackObjectWIthData : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent.GetComponentInChildren<UnityEngine.Video.VideoPlayer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player)
+        {
+            Debug.Log("Got frame"+ player.frame);
+        }
+    }
+}
+
+
+//public class TrackingData
+//{
+//    public Dictionary<string, List<Vector2>> data = new Dictionary<string, List<Vector2>>()
+//    {
+//        {
+//            "henk", new List<Vector2>()
+//            {
+//                new Vector2(0,0),
+//                new Vector2(0,5),
+//            }
+//        }
+//    };
+    
+
+//    void bla()
+//    {
+//        //new ArrayList()
+//        List < Vector2 > a = new List<Vector2>();
+//        a.Add(new Vector2(0, 0));
+//        data.Add("henk", a);
+//    }
+//}
