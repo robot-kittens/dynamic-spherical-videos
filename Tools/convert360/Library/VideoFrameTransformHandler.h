be72477af73f102429bc6b785ba5b2df56191e40
Patrick van der Werf <patrick@robotkittens.nl>
2021-01-29 15:44:25 +0100

    Reworked library

---
+++ Tools/convert360/Library/VideoFrameTransformHandler.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "VideoFrameTransformHelper.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct VideoFrameTransform VideoFrameTransform;
+
+extern VideoFrameTransform* VideoFrameTransform_new(
+  FrameTransformContext* ctx);
+
+extern void VideoFrameTransform_delete(VideoFrameTransform* transform);
+
+extern int VideoFrameTransform_generateMapForPlane(
+  VideoFrameTransform* transform,
+  int inputWidth,
+  int inputHeight,
+  int outputWidth,
+  int outputHeight,
+  int transformMatPlaneIndex);
+
+extern int VideoFrameTransform_transformFramePlane(
+  VideoFrameTransform* transform,
+  uint8_t* inputData,
+  uint8_t* outputData,
+  int inputWidthWithPadding,
+  int inputHeight,
+  int outputWidth,
+  int outputHeight,
+  int outputWidthWithPadding,
+  int transformMatPlaneIndex,
+  int imagePlaneIndex);
+
+#ifdef __cplusplus
+}
+#endif
